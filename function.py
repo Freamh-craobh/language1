23ef6590d919bd0a2c94c3ed07839af0422647ab
Freamh-craobh <oisin.duke@gmail.com>
2026-01-25 22:30:18 +0000

    code for detecting CV violations, and inserting vowel, vowel selection incomplete

--- function.py
+++ function.py
@@ -15,3 +15,32 @@ def random_call(min, max):
     random.seed(rand_seed)
     rand_seed += 1
     return random.randrange(min, max)
+
+def weights(vowel_rates):
+    total = 0
+    limits = []
+    for i in vowel_rates:
+        limits.append(total + int((1/i)*100))
+        total += int((1/i)*100)
+    
+    rand_limit = random_call(0, total)
+    print(limits, rand_limit)
+    for i in range(len(limits)):
+        if rand_limit < limits[i-1]:
+            return i # THIS SHOULD BE DONE
+   
+
+def vowel_selection():
+    vowel_rates = {}
+    with open("vowels.txt", "r") as f:
+        for i,line in enumerate(f.readlines()):
+         if i > 0:
+            vowel_rates[i] = line
+        print(vowel_rates) #NEED TO FINISH THIS AFTER WEIGHTS FUNC IS DONE
+    
+
+def dict_lister(self, addition, list_item):
+    if addition not in self:
+        self[addition] = []
+    self[addition] = [list_item]
+    
--- main.py
+++ main.py
@@ -1,4 +1,88 @@
-import random, function
+import random, function, io
 
-values = [function.random_call(1, 10) for i in range(500)]
-print(values)
+#values = [function.random_call(1, 10) for i in range(500)]
+#print(values)
+
+#set up consonant and vowel lists
+test = True
+consonants = []
+vowels = []
+
+with open("consonants.txt", "r") as f:
+    for i,line in enumerate(f.readlines()):
+        if i > 0:
+            consonants = [i for i in line if i != ","]
+        
+with open("vowels.txt", "r") as f:
+    for i,line in enumerate(f.readlines()):
+        if i > 0:
+            vowels += [i for i in line if i != ","]
+            try:
+                vowels.remove("\n")
+            except:
+                continue
+
+print(vowels)
+with open("input.txt","r") as f:
+    if test:
+        for i,line in enumerate(f.readlines()):
+            if i < 1:
+                input = line
+    else: 
+        input = f.read()
+
+
+wordlist = input.strip().split(sep=" ")
+
+output = []
+
+violations = {}
+
+
+print(wordlist)
+
+
+for word in wordlist:
+    for i, char in enumerate(word):
+        if char in vowels:
+            continue
+        elif char in consonants:
+            try:
+                if word[(i+1)] in vowels or word[(i+1)] == "h":
+                    continue
+                else:
+                    #if char == "h":
+                    #    continue
+                        #print(f"{word[i-1]}{char} in {word} interal CV violation")
+                    #else: 
+                        #print(f"{char} in {word} interal CV violation")
+                    if word not in violations:
+                        violations[word] = []
+                    violations[word] += [i]
+            except:
+                if char == "h":
+                        continue
+                        #print(f"{word[i-1]}{char} in {word} final consonant")
+                else:
+                    #print(f"{char} in {word} final consonant")
+                    if word not in violations:
+                        violations[word] = []
+                    violations[word] += [i]
+        else:
+            print(f"{char} in {word} character error")
+            raise Exception("Glyph not recognised, add to consonant/vowel list")
+
+        #print(violations)    
+
+
+
+for key, positions in violations.items():
+    print(key, positions)
+    replaced = key
+    for pos in sorted(positions, reverse=True):
+        replaced = replaced[:pos+1] + "a" + replaced[pos+1:]
+    print(key, replaced)
+
+function.vowel_selection() #TESTING
+
+print(function.weights({1:"a", 2:"b", 3:"c"})) #TESTING WEIGHTS
